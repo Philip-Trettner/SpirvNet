5a5a70f9f4833a2379b24e76ee8b14bc4a17b722
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2015-03-19 21:13:38 +0100

    Cecil-Loader and method lookup

---
+++ SpirvNet/SpirvNet/DotNet/CecilLoader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Collections.Generic;
+
+namespace SpirvNet.DotNet
+{
+    /// <summary>
+    /// Static loader for assemblies in cecil
+    /// </summary>
+    public static class CecilLoader
+    {
+        /// <summary>
+        /// Cache
+        /// </summary>
+        private static readonly Dictionary<Assembly, AssemblyDefinition[]> Assemblies = new Dictionary<Assembly, AssemblyDefinition[]>();
+
+        /// <summary>
+        /// Returns all assembly definitions of a given assembly (caches result)
+        /// </summary>
+        public static AssemblyDefinition[] DefinitionFor(Assembly asm)
+        {
+            if (asm == null)
+                return new AssemblyDefinition[] { };
+
+            AssemblyDefinition[] definitions;
+            if (Assemblies.TryGetValue(asm, out definitions))
+                return definitions;
+
+            definitions = asm.GetFiles().Select(AssemblyDefinition.ReadAssembly).ToArray();
+            Assemblies.Add(asm, definitions);
+            return definitions;
+        }
+
+        /// <summary>
+        /// Returns the definition for a given method
+        /// (Searches through the type and should therefore not be used often)
+        /// </summary>
+        public static MethodDefinition DefinitionFor(MethodInfo method)
+        {
+            var type = method.DeclaringType;
+            var mname = method.Name;
+            var rettypename = method.ReturnType.FullName;
+            var paratypename = method.GetParameters().Select(p => p.ParameterType.FullName).ToArray();
+            var defs = DefinitionFor(type?.Assembly);
+
+            foreach (var def in defs)
+                foreach (var module in def.Modules)
+                    foreach (var func in module.GetType(type?.FullName)?.Methods ?? new Collection<MethodDefinition>())
+                        if (func.Name == mname && 
+                            func.ReturnType.FullName == rettypename &&
+                            paratypename.SequenceEqual(func.Parameters.Select(p => p.ParameterType.FullName)))
+                            return func;
+
+            throw new KeyNotFoundException("Could not locate " + method.Name);
+        }
+
+        /// <summary>
+        /// Returns the definition for a given method
+        /// (Searches through the type and should therefore not be used often)
+        /// </summary>
+        public static MethodDefinition DefinitionFor(Delegate d) => DefinitionFor(d.Method);
+    }
+}
---
+++ SpirvNet/SpirvNet/Tests/CecilTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SpirvNet.DotNet;
+
+namespace SpirvNet.Tests
+{
+    [TestFixture]
+    public class CecilTest
+    {
+        [Test]
+        public void MethodTest()
+        {
+            var def0 = CecilLoader.DefinitionFor((Action)MethodTest);
+            Assert.AreEqual("MethodTest", def0.Name);
+
+            var def1 = CecilLoader.DefinitionFor((Func<double, double>)Math.Abs);
+            Assert.AreEqual("Abs", def1.Name);
+            Assert.AreEqual("Double", def1.ReturnType.Name);
+            Assert.AreEqual(1, def1.Parameters.Count);
+            Assert.AreEqual("Double", def1.Parameters[0].ParameterType.Name);
+        }
+    }
+}
